e66f13e09e28a0630fcec0cbad02fa5926ef48b8
Unger Andrey <unger1984@gmail.com>
2023-02-13 17:57:49 +0300

    skills page

--- lib/src/presentation/widgets/skills/skills_page.dart
+++ lib/src/presentation/widgets/skills/skills_page.dart
@@ -3,6 +3,7 @@ import 'package:portfolio/generated/l10n.dart';
 import 'package:portfolio/src/presentation/styles/styles.dart';
 import 'package:portfolio/src/presentation/widgets/common/main_menu.dart';
 import 'package:portfolio/src/presentation/widgets/skills/skill_item.dart';
+import 'package:portfolio/src/utils/const.dart';
 
 @immutable
 class SkillsPage extends StatelessWidget {
@@ -20,55 +21,58 @@ class SkillsPage extends StatelessWidget {
           child: Center(
             child: ConstrainedBox(
               constraints: BoxConstraints.tightFor(width: 800),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(top: 20.0),
-                    child: MainMenu(),
-                  ),
-                  SizedBox(height: 20),
-                  Text(
-                    S.current.skills_1,
-                    style: TextStyle(fontFamily: 'Montserrat', fontWeight: FontWeight.w700, fontSize: 30),
-                  ),
-                  Wrap(
-                    children: [
-                      SkillItem(title: 'HTML5', img: 'html'),
-                      SkillItem(title: 'CSS3', img: 'css'),
-                      SkillItem(title: 'JavaScript', img: 'js'),
-                      SkillItem(title: 'TypeScript', img: 'ts'),
-                      SkillItem(title: 'SASS', img: 'sass'),
-                    ],
-                  ),
-                  Text(
-                    S.current.skills_2,
-                    style: TextStyle(fontFamily: 'Montserrat', fontWeight: FontWeight.w700, fontSize: 30),
-                  ),
-                  Wrap(
-                    children: [
-                      SkillItem(title: 'Flutter', img: 'flutter'),
-                      SkillItem(title: 'React Native', img: 'rn'),
-                    ],
-                  ),
-                  Text(
-                    S.current.skills_3,
-                    style: TextStyle(fontFamily: 'Montserrat', fontWeight: FontWeight.w700, fontSize: 30),
-                  ),
-                  Wrap(
-                    children: [
-                      SkillItem(title: 'NodeJS', img: 'node'),
-                      SkillItem(title: 'TypeScript', img: 'ts'),
-                      SkillItem(title: 'GoLang', img: 'go'),
-                      SkillItem(title: 'PostgresSQL', img: 'postgres'),
-                      SkillItem(title: 'MySQL', img: 'mysql'),
-                      SkillItem(title: 'MongoDB', img: 'mongo'),
-                      SkillItem(title: 'Dart', img: 'dart'),
-                      SkillItem(title: 'Docker', img: 'docker'),
-                    ],
-                  ),
-                ],
-              ),
+              child: LayoutBuilder(builder: (context, constrain) {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    if (constrain.maxWidth > widthMobile)
+                      Padding(
+                        padding: const EdgeInsets.only(top: 20.0),
+                        child: MainMenu(),
+                      ),
+                    SizedBox(height: 20),
+                    Text(
+                      S.current.skills_1,
+                      style: TextStyle(fontFamily: 'Montserrat', fontWeight: FontWeight.w700, fontSize: 30),
+                    ),
+                    Wrap(
+                      children: [
+                        SkillItem(title: 'HTML5', img: 'html'),
+                        SkillItem(title: 'CSS3', img: 'css'),
+                        SkillItem(title: 'JavaScript', img: 'js'),
+                        SkillItem(title: 'TypeScript', img: 'ts'),
+                        SkillItem(title: 'SASS', img: 'sass'),
+                      ],
+                    ),
+                    Text(
+                      S.current.skills_2,
+                      style: TextStyle(fontFamily: 'Montserrat', fontWeight: FontWeight.w700, fontSize: 30),
+                    ),
+                    Wrap(
+                      children: [
+                        SkillItem(title: 'Flutter', img: 'flutter'),
+                        SkillItem(title: 'React Native', img: 'rn'),
+                      ],
+                    ),
+                    Text(
+                      S.current.skills_3,
+                      style: TextStyle(fontFamily: 'Montserrat', fontWeight: FontWeight.w700, fontSize: 30),
+                    ),
+                    Wrap(
+                      children: [
+                        SkillItem(title: 'NodeJS', img: 'node'),
+                        SkillItem(title: 'TypeScript', img: 'ts'),
+                        SkillItem(title: 'GoLang', img: 'go'),
+                        SkillItem(title: 'PostgresSQL', img: 'postgres'),
+                        SkillItem(title: 'MySQL', img: 'mysql'),
+                        SkillItem(title: 'MongoDB', img: 'mongo'),
+                        SkillItem(title: 'Dart', img: 'dart'),
+                        SkillItem(title: 'Docker', img: 'docker'),
+                      ],
+                    ),
+                  ],
+                );
+              }),
             ),
           ),
         ),
